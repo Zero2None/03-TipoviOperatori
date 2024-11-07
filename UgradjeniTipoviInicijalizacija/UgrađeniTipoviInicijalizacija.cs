ae4ea316cb5cf522ba5bd5a1103400454de08c2c
Ivan Ceranac <ivcerana@vsite.hr>
2024-11-07 20:50:31 +0100

    resolved TODO 010, 013,014

--- UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
+++ UgradjeniTipoviInicijalizacija/UgrađeniTipoviInicijalizacija.cs
@@ -5,15 +5,18 @@
         static void Main()
         {
             // TODO:010 Inicijalizirati varijablu tipa int pozivom podrazumijevanog (praznog) konstruktora te ispisati vrijednost varijable.
+            int a=5;
+            
+            Console.WriteLine(a);
 
             // TODO:011 Inicijalizirati drugu varijablu tipa int pozivom konstruktora koji kao argument prima broj 4. Provjeriti dozvoljava li to prevoditelj.
 
             // TODO:012 Inicijalizirati varijablu tipa string pozivom podrazumijevanog konstruktora.Provjeriti dozvoljava li to prevoditelj.
 
             // TODO:013 Provjeriti koji su mogući konstruktori za string.
-
+            string s2 = new string(string.Empty);
             // TODO:014 Napisati kod koji inicijalizira novu varijablu tipa string na vrijednost "string 1".
-
+            string s1 = "string 1";
             Console.WriteLine("GOTOVO!!!");
         }
     }
